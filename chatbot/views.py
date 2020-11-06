742795b17bbfff52481d999793cacfe0f2364cb2
Tanvi Aditya <tanviaditya1@gmail.com>
2020-11-06 16:04:56 +0530

    Language selection

--- chatbot/views.py
+++ chatbot/views.py
@@ -6,14 +6,32 @@ import pandas as pd
 import random
 from chatbot.chat_file import chatbot
 from googletrans import Translator
+import http.client
+import json
+from time import sleep
+
 
 # Create your views here.
+def translateText(text,src,dest):
+    result=None
+    translator = Translator()
+    while result == None:
+        try:
+            result = translator.translate(text, src=src, dest=dest).text
+        except Exception as e:
+            # print(e)
+            translator = Translator()
+            sleep(0.5)
+            pass
+    return result      
+
 class translate(APIView):
     def get(self,request):
         if request.method == 'GET':
             text = request.GET.get('text')
-            translator = Translator()
-            text=translator.translate(text, src='gu', dest='en').text
+            lang=request.GET.get('lang')
+            # translator = Translator()
+            text=translateText(text,lang,'en')
             return JsonResponse({'text':text})
 
 class call_model(APIView):
@@ -21,9 +39,7 @@ class call_model(APIView):
     def get(self,request):
         if request.method == 'GET':
             data = request.GET.get('data')
-            # lang=request.GET.get('lang')
-            lang='gu'
-            
+            lang=request.GET.get('lang')
             symp=chatbot.symptomDetector(data)
             print(data)
             translator = Translator()
@@ -39,26 +55,26 @@ class call_model(APIView):
             if data in greetings:
                 reply=random.choice(greetings_response)
                 if(lang!='en'):
-                    reply=translator.translate(reply, src='en', dest=lang).text
+                    reply=translateText(reply,'en',lang)
             elif data in exit_list:
                 reply=random.choice(exit_respose)
                 if(lang!='en'):
-                    reply=translator.translate(reply, src='en', dest=lang).text
+                    reply=translateText(reply,'en',lang)
             else:
                 if len(symp)>0:
                     if len(symp)<3:
                         reply="Please enter few more symptoms"
                         if(lang!='en'):
-                            reply=translator.translate(reply, src='en', dest=lang).text
+                            reply=translateText(reply,'en',lang)
                     else:
                         predict=ChatbotConfig.pickled_model.predict([final_input])
                         reply="You might have "+(predict[0])
                         if(lang!='en'):
-                            reply=translator.translate(reply, src='en', dest=lang).text
+                            reply=translateText(reply,'en',lang)
                         d=chatbot.getDescription(predict[0])
                         d=d['Description'].values[0]
                         if(lang!='en'):
-                            d=translator.translate(d, src='en', dest=lang).text
+                            d=translateText(d,'en',lang)
                         r=chatbot.getPrecautions(predict[0])
                         r1="1] "+r['Precaution_1'].values[0].title()
                         r2="2] "+r['Precaution_2'].values[0].title()
@@ -66,13 +82,12 @@ class call_model(APIView):
                         r4="4] "+r['Precaution_4'].values[0].title()
                         p=r1+'\n'+r2+'\n'+r3+'\n'+r4
                         if(lang!='en'):
-                            p=translator.translate(p, src='en', dest=lang).text
+                            p=translateText(p,'en',lang)
                 else:
                     reply="Please enter symptoms you are facing"   
                     if(lang!='en'):
-                        reply=translator.translate(reply, src='en', dest=lang).text
+                        reply=translateText(reply,'en',lang)
             return JsonResponse({'data':reply,
                                 'description':d,
                                 'precautions':p
                                 })
-                
---
+++ sample.py
@@ -0,0 +1,64 @@
+# import http.client
+# import json
+# # import jsonurl
+# conn = http.client.HTTPSConnection("google-translate1.p.rapidapi.com")
+
+# payload ={
+#     "q":"Hello world",
+#     "source":"en",
+#     "target":"gu",
+# }
+# # payload=JSON.stringify(payload)
+# # "q=Hello%2C%20world!&source=en&target=gu"
+# # payload = jsonurl.query_string(payload)
+# headers = {
+#     'content-type': "application/x-www-form-urlencoded",
+#     'accept-encoding': "application/gzip",
+#     'x-rapidapi-key': "acceac1621msh846fafdf56d7872p1224d8jsn37b1f64ff23e",
+#     'x-rapidapi-host': "google-translate1.p.rapidapi.com"
+#     }
+
+# conn.request("POST", "/language/translate/v2",payload,headers)
+
+# res = conn.getresponse()
+
+# data = res.read()
+
+# print(data.decode("utf-8"))
+
+
+# # import json
+
+# # # a Python object (dict):
+# # x = {
+# #     "q":"Hello world",
+# #     "source":"en",
+# #     "target":"gu",
+# # }
+
+# # # convert into JSON:
+# # y = json.dumps(x,separators=(',', ':'))
+# # # json_mylist = json.dumps(mylist, separators=(',', ':'))
+
+# # # the result is a JSON string:
+# # print(y)
+
+from googletrans import Translator
+from time import sleep
+text="I have fever"
+result=None
+translator = Translator()
+while result == None:
+    try:
+        result = translator.translate(text, src='en', dest='hi').text
+    except Exception as e:
+        # print(e)
+        translator = Translator()
+        sleep(0.5)
+        pass      
+
+
+# text=translator.translate(text, src='en', dest='hi').text
+print(result)
+
+
